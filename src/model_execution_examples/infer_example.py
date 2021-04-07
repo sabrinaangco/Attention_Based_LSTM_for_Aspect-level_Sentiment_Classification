73937df2daaa4e9b09316810032af112a74fccb0
sabrinaangco <sabrina.angco@ryerson.ca>
2021-04-07 17:49:30 -0400

    commit

---
+++ src/model_execution_examples/infer_example.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn.functional as F
+import argparse
+import numpy as np
+from utils.data_utils import build_tokenizer, build_embedding_matrix, pad_and_truncate
+from models import LSTM, TD_LSTM, TC_LSTM, ATAE_LSTM
+
+from dependency_graph import dependency_adj_matrix
+
+
+class Inferer:
+    """A simple inference example"""
+    def __init__(self, opt):
+        self.opt = opt
+       
+        self.tokenizer = build_tokenizer(
+            fnames=[opt.dataset_file['train'], opt.dataset_file['test']],
+            max_seq_len=opt.max_seq_len,
+            dat_fname='{0}_tokenizer.dat'.format(opt.dataset))
+        embedding_matrix = build_embedding_matrix(
+            word2idx=self.tokenizer.word2idx,
+            embed_dim=opt.embed_dim,
+            dat_fname='{0}_{1}_embedding_matrix.dat'.format(str(opt.embed_dim), opt.dataset))
+        self.model = opt.model_class(embedding_matrix, opt)
+        print('loading model {0} ...'.format(opt.model_name))
+        self.model.load_state_dict(torch.load(opt.state_dict_path))
+        self.model = self.model.to(opt.device)
+        # switch model to evaluation mode
+        self.model.eval()
+        torch.autograd.set_grad_enabled(False)
+
+    def evaluate(self, text, aspect):
+        aspect = aspect.lower().strip()
+        text_left, _, text_right = [s.strip() for s in text.lower().partition(aspect)]
+        
+        text_indices = self.tokenizer.text_to_sequence(text_left + " " + aspect + " " + text_right)
+        context_indices = self.tokenizer.text_to_sequence(text_left + " " + text_right)
+        left_indices = self.tokenizer.text_to_sequence(text_left)
+        left_with_aspect_indices = self.tokenizer.text_to_sequence(text_left + " " + aspect)
+        right_indices = self.tokenizer.text_to_sequence(text_right, reverse=True)
+        right_with_aspect_indices = self.tokenizer.text_to_sequence(aspect + " " + text_right, reverse=True)
+        aspect_indices = self.tokenizer.text_to_sequence(aspect)
+        left_len = np.sum(left_indices != 0)
+        aspect_len = np.sum(aspect_indices != 0)
+        aspect_boundary = np.asarray([left_len, left_len + aspect_len - 1], dtype=np.int64)
+
+        text_len = np.sum(text_indices != 0)
+        concat_segments_indices = [0] * (text_len + 2) + [1] * (aspect_len + 1)
+        concat_segments_indices = pad_and_truncate(concat_segments_indices, self.tokenizer.max_seq_len)
+
+
+        dependency_graph = dependency_adj_matrix(text)
+
+        data = {
+            'concat_segments_indices': concat_segments_indices,
+
+            'text_indices': text_indices,
+            'context_indices': context_indices,
+            'left_indices': left_indices,
+            'left_with_aspect_indices': left_with_aspect_indices,
+            'right_indices': right_indices,
+            'right_with_aspect_indices': right_with_aspect_indices,
+            'aspect_indices': aspect_indices,
+            'aspect_boundary': aspect_boundary,
+            'dependency_graph': dependency_graph,
+        }
+
+        t_inputs = [torch.tensor([data[col]], device=self.opt.device) for col in self.opt.inputs_cols]
+        t_outputs = self.model(t_inputs)
+        t_probs = F.softmax(t_outputs, dim=-1).cpu().numpy()
+
+        return t_probs
+
+def run_infer_example(example_sentence = 'the service was bad', aspect = 'service', n_model = 'atae_lstm', n_dataset = 'restaurant', model_file = 'atae_lstm_restaurant_val_acc_0.7411_e8'):
+
+# if __name__ == '__main__':
+    model_classes = {
+        'lstm': LSTM,
+        'td_lstm': TD_LSTM,
+        'tc_lstm': TC_LSTM,
+        'atae_lstm': ATAE_LSTM,
+
+    }
+    dataset_files = {
+        'twitter': {
+            'train': './datasets/acl-14-short-data/train.raw',
+            'test': './datasets/acl-14-short-data/test.raw'
+        },
+        'restaurant': {
+            'train': './datasets/semeval14/Restaurants_Train.xml.seg',
+            'test': './datasets/semeval14/Restaurants_Test_Gold.xml.seg'
+        },
+        'laptop': {
+            'train': './datasets/semeval14/Laptops_Train.xml.seg',
+            'test': './datasets/semeval14/Laptops_Test_Gold.xml.seg'
+        }
+    }
+    input_colses = {
+        'lstm': ['text_indices'],
+        'td_lstm': ['left_with_aspect_indices', 'right_with_aspect_indices'],
+        'tc_lstm': ['left_with_aspect_indices', 'right_with_aspect_indices', 'aspect_indices'],
+        'atae_lstm': ['text_indices', 'aspect_indices'],
+      
+    }
+    class Option(object): pass
+    opt = Option()
+    opt.model_name = n_model
+    opt.model_class = model_classes[opt.model_name]
+    opt.dataset = n_dataset
+    opt.dataset_file = dataset_files[opt.dataset]
+    opt.inputs_cols = input_colses[opt.model_name]
+    # set your trained models here
+    opt.state_dict_path = 'output/train/state_dict/'+ model_file
+    opt.embed_dim = 300
+    opt.hidden_dim = 300
+    opt.max_seq_len = 85
+ 
+    opt.polarities_dim = 3
+    opt.hops = 3
+    opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    opt.local_context_focus = 'cdm'
+    opt.SRD = 3
+
+    inf = Inferer(opt)
+    t_probs = inf.evaluate(example_sentence, aspect)
+    print(t_probs)
+#     print(t_probs.argmax(axis=-1))
+    print(t_probs.argmax(axis=-1) - 1)
---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+
+from models.lstm import LSTM
+
+from models.td_lstm import TD_LSTM
+from models.tc_lstm import TC_LSTM
+from models.atae_lstm import ATAE_LSTM
+
